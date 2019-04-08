e22aed91cbd02135c3c542846589aa8ce61f88fc
Cozzone, Justin Michael <jcozzone3@gatech.edu>
2019-04-08 16:05:31 -0400

    Chart Update
    Dropdown is working, charts are displaying. Points, etc, and axes shifting obviously not updated yet. Good progress so far, though.

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var height = 500;
 
 var selectedId = 10000;
 
-var selectedValueY;
+var selectedValueY = "Admission Rate";
 
 /***************************************************************************
                             DATA SETUP
@@ -18,12 +18,12 @@ d3.csv("data/colleges.csv", function(data) {
 
         d.admission = Number(d['Admission Rate']);
 
-        d.act = Number(d['ACT Median']);
-        d.sat = Number(d['SAT Average']);
+        // d.act = Number(d['ACT Median']);
+        // d.sat = Number(d['SAT Average']);
 
         d.undergrads = Number(d['Undergrad Population']);
-        d.partTime = Number(d['% Part-time Undergrads']);
-        d.fullTime = Number(1.0 - Number(d['% Part-time Undergrads']));
+        // d.partTime = Number(d['% Part-time Undergrads']);
+        // d.fullTime = Number(1.0 - Number(d['% Part-time Undergrads']));
         d.over25 = Number(d['% Undergrads 25+ y.o.']);
         d.under25 = Number(1.0 - Number(d['% Undergrads 25+ y.o.']));
 
@@ -46,41 +46,39 @@ d3.csv("data/colleges.csv", function(data) {
         d.avgCost = Number(d['Average Cost']);
         d.studentExpenditure = Number(d['Expenditure Per Student']);
         d.medianDebt = Number(d['Median Debt']);
-        d.medianDebtGrad = Number(d['Median Debt on Graduation']);
-        d.medianDebtWithdraw = Number(d['Median Debt on Withdrawal']);
+        // d.medianDebtGrad = Number(d['Median Debt on Graduation']);
+        // d.medianDebtWithdraw = Number(d['Median Debt on Withdrawal']);
 
-        d.avgFacultySalary = Number(d['Average Faculty Salary']);
-        d.fullTimeFaculty = Number(d['% Full-time Faculty']);
-        d.partTimeFaculty = Number(1.0 - Number(d['% Full-time Faculty']));
+        // d.avgFacultySalary = Number(d['Average Faculty Salary']);
+        // d.fullTimeFaculty = Number(d['% Full-time Faculty']);
+        // d.partTimeFaculty = Number(1.0 - Number(d['% Full-time Faculty']));
 
         d.undergradsWithPell = Number(d['% Undergrads with Pell Grant']);
         d.undergradsNoPell = Number(1.0 - Number(d['% Undergrads with Pell Grant']));
 
-        d.completion150 = Number(d['Completion Rate 150% time']);
-        d.threeYearDefault = Number(d['3 Year Default Rate']);
+        // d.completion150 = Number(d['Completion Rate 150% time']);
+        // d.threeYearDefault = Number(d['3 Year Default Rate']);
 
-        d.retention = Number(d['Retention Rate (First Time Students)']);
+        // d.retention = Number(d['Retention Rate (First Time Students)']);
 
         d.fedLoans = Number(d['% Federal Loans']);
-        d.pellGrant = Number(d['% Pell Grant Recipients']);
+        // d.pellGrant = Number(d['% Pell Grant Recipients']);
 
         d.unemployedAfter8 = Number(d['Number of Unemployed 8 years after entry']);
         d.employedAfter8 = Number(d['Number of Employed 8 years after entry']);
 
-        d.meanEarningsAfter8 = Number(d['Mean Earnings 8 years After Entry']);
+        // d.meanEarningsAfter8 = Number(d['Mean Earnings 8 years After Entry']);
         d.medEarningsAfter8 = Number(d['Median Earnings 8 years After Entry']);
 
-        d.highestDegree = Number(d['Highest Degree']);
+        // d.highestDegree = Number(d['Highest Degree']);
     })
-    console.log(data);
-
 
     var admissionExtent = d3.extent(data, function(row) { return row.admission; });
-    var actExtent = d3.extent(data, function(row) { return row.act; });
-    var satExtent = d3.extent(data,  function(row) { return row.sat;  });
+    // var actExtent = d3.extent(data, function(row) { return row.act; });
+    // var satExtent = d3.extent(data,  function(row) { return row.sat;  });
     var undergradsExtent = d3.extent(data,  function(row) {return row.undergrads;   });
-    var partTimeExtent = d3.extent(data, function(row) { return row.partTime; });
-    var fullTimeExtent = d3.extent(data, function(row) { return row.fullTime; });
+    // var partTimeExtent = d3.extent(data, function(row) { return row.partTime; });
+    // var fullTimeExtent = d3.extent(data, function(row) { return row.fullTime; });
     var over25Extent = d3.extent(data, function(row) { return row.over25; });
     var under25Extent = d3.extent(data, function(row) { return row.under25; });
     var avgAgeExtent = d3.extent(data, function(row) { return row.avgAge; });
@@ -98,56 +96,42 @@ d3.csv("data/colleges.csv", function(data) {
     var avgCostExtent = d3.extent(data, function(row) { return row.avgCost; });
     var studentExpenditureExtent = d3.extent(data, function(row) { return row.studentExpenditure; });
     var medianDebtExtent = d3.extent(data, function(row) { return row.medianDebt; });
-    var medianDebtGradExtent = d3.extent(data, function(row) { return row.medianDebtGrad; });
-    var medianDebtWithdrawExtent = d3.extent(data, function(row) { return row.medianDebtWithdraw; });
-    var avgFacultySalaryExtent = d3.extent(data, function(row) { return row.avgFacultySalary; });
-    var fullTimeFacultyExtent = d3.extent(data, function(row) { return row.fullTimeFaculty; });
-    var partTimeFacultyExtent = d3.extent(data, function(row) { return row.partTimeFaculty; });
+    // var medianDebtGradExtent = d3.extent(data, function(row) { return row.medianDebtGrad; });
+    // var medianDebtWithdrawExtent = d3.extent(data, function(row) { return row.medianDebtWithdraw; });
+    // var avgFacultySalaryExtent = d3.extent(data, function(row) { return row.avgFacultySalary; });
+    // var fullTimeFacultyExtent = d3.extent(data, function(row) { return row.fullTimeFaculty; });
+    // var partTimeFacultyExtent = d3.extent(data, function(row) { return row.partTimeFaculty; });
     var undergradsWithPellExtent = d3.extent(data, function(row) { return row.undergradsWithPell; });
     var undergradsNoPellExtent = d3.extent(data, function(row) { return row.undergradsNoPell; });
-    var completion150Extent = d3.extent(data, function(row) { return row.completion150; });
-    var threeYearDefaultExtent = d3.extent(data, function(row) { return row.threeYearDefault; });
-    var retentionExtent = d3.extent(data, function(row) { return row.retention; });
+    // var completion150Extent = d3.extent(data, function(row) { return row.completion150; });
+    // var threeYearDefaultExtent = d3.extent(data, function(row) { return row.threeYearDefault; });
+    // var retentionExtent = d3.extent(data, function(row) { return row.retention; });
     var fedLoansExtent = d3.extent(data, function(row) { return row.fedLoans; });
     var pellGrantExtent = d3.extent(data, function(row) { return row.pellGrant; });
     var unemployedAfter8Extent = d3.extent(data, function(row) { return row.unemployedAfter8; });
     var employedAfter8Extent = d3.extent(data, function(row) { return row.employedAfter8; });
-    var meanEarningsAfter8Extent = d3.extent(data, function(row) { return row.meanEarningsAfter8; });
+    // var meanEarningsAfter8Extent = d3.extent(data, function(row) { return row.meanEarningsAfter8; });
     var medEarningsAfter8Extent = d3.extent(data, function(row) { return row.medEarningsAfter8; });
-    var highestDegreeExtent = d3.extent(data, function(row) { return row.highestDegree; });
+    // var highestDegreeExtent = d3.extent(data, function(row) { return row.highestDegree; });
 
-    // var satExtents = {
-	// "SATM": satmExtent,
-	// "SATV": satvExtent }
-
-    });
 /****************************************************************************
-                        DROPDOWN STUFF - WILL NEED TO EDIT
+                                DROPDOWN STUFF
 
 ****************************************************************************/
-// var yOptions = [];
-// var dd = d3.select(wrapper) //?
-//         .append('p')
-//         .append('select')
-//         .attr('class', 'select')
-//         .on('change', onchange)
-//         .selectAll('option')
-//         .data(yOptions).enter()
-//         .append('option')
-//         .text(function (d) { return d; });
+    var yOptions = ["Admission Rate", "Number of Undergraduates", "Percentage of Undergraduates Over 25", "Percentage of Undergraduates Under 25", "Average Age of Entry", "Average Family Income", "Median Family Income", "Poverty Rate", "Percent White", "Percent Black", "Percent Hispanic", "Percent Asian", "Percent American Indian", "Percent Pacific Islander", "Percent Biracial", "Percent Aliens", "Average Cost", "Student Expenditures", "Percent Undergraduates with Pell Grant", "Percent Undergraduates without Pell Grant", "Percentage of Undergraduates with a Federal Loan", "Percentage of Undergraduates Unemployed After 8 Years", "Percentage of Undergraduates Employed After 8 Years"];
+    var dd = d3.select(wrapper) //?
+            .append('p')
+            .append('select')
+            .attr('class', 'select')
+            .on('change', onchange)
+            .selectAll('option')
+            .data(yOptions).enter()
+            .append('option')
+            .text(function (d) { return d; });
 
-//     function onchange() {
-    // change x scale and redo points here?
-//         selectedValueY = d3.select('select').property('value');
-
-//         if (selectValue === 'steelblue') {
-//             bars.selectAll('rect')
-//                 .style('fill', function(d) { return 'steelblue'; });
-//         } else {
-//             bars.selectAll('rect')
-//                 .style('fill', function(d) { return selectValue; });
-//         }
-//     }
+    function onchange() {
+        selectedValueY = d3.select('select').property('value');
+    }
 
 /****************************************************************************
                                 AXES SETUP
@@ -156,30 +140,29 @@ d3.csv("data/colleges.csv", function(data) {
     this shouldn't be too bad, just grunt work
 *****************************************************************************/
     // // Axis setup
-    // var xScale = d3.scaleLinear().domain(medianDebtExtent).range([50, 470]);
-    // var yScale = d3.scaleLinear().domain(satvExtent).range([470, 30]);
-    //NEED TO HAVE ySCALE STUFF CHANGE WITH DD
+    var xScale = d3.scaleLinear().domain(medianDebtExtent).range([50, 470]);
+    var yScale = d3.scaleLinear().domain(admissionExtent).range([470, 30]);
 
-    // var xScale2 = d3.scaleLinear().domain(medEarningsAfter8Extent).range([50, 470]);
-    // var yScale2 = d3.scaleLinear().domain(gpaExtent).range([470, 30]);
+    var xScale2 = d3.scaleLinear().domain(medEarningsAfter8Extent).range([50, 470]);
+    var yScale2 = d3.scaleLinear().domain(admissionExtent).range([470, 30]);
 
-    // var xAxis = d3.axisBottom().scale(xScale);
-    // var yAxis = d3.axisLeft().scale(yScale);
+    var xAxis = d3.axisBottom().scale(xScale);
+    var yAxis = d3.axisLeft().scale(yScale);
 
-    // var xAxis2 = d3.axisBottom().scale(xScale2);
-    // var yAxis2 = d3.axisLeft().scale(yScale2);
+    var xAxis2 = d3.axisBottom().scale(xScale2);
+    var yAxis2 = d3.axisLeft().scale(yScale2);
 
-    // //Create SVGs for charts
-    // var chart1 = d3.select("#chart1")
-	   //              .append("svg:svg")
-	   //              .attr("width",width)
-	   //              .attr("height",height);
+    //Create SVGs for charts
+    var chart1 = d3.select("#chart1")
+	                .append("svg:svg")
+	                .attr("width",width)
+	                .attr("height",height);
 
 
-    // var chart2 = d3.select("#chart2")
-	   //              .append("svg:svg")
-	   //              .attr("width",width)
-	   //              .attr("height",height);
+    var chart2 = d3.select("#chart2")
+	                .append("svg:svg")
+	                .attr("width",width)
+	                .attr("height",height);
 
 /*****************************************************************************
                             POINTS SETUP
@@ -188,18 +171,18 @@ d3.csv("data/colleges.csv", function(data) {
     I am missing. However, once again, shouldn't be awful, just a whole
     bunch of grunt work.
 
-                        WILL ALSO NEED BRUSHING
+                        WILL ALSO NEED BRUSHING?
 *****************************************************************************/
 
 	 //add scatterplot points
  //     var temp1= chart1.selectAll("circle")
-	//    .data(csv)
+	//    .data(data)
 	//    .enter()
 	//    .append("circle")
 	//    .attr("id",function(d,i) {return "i" + i;} )
 	//    .attr("stroke", "black")
  //       .attr("class", "unselected")
-	//    .attr("cx", function(d) { return xScale(d.SATM); })
+	//    .attr("cx", function(d) { return xScale(d.medianDebt); })
 	//    .attr("cy", function(d) { return yScale(d.SATV); })
 	//    .attr("r", 5)
 	//    .on("click", function(d,i){
@@ -233,13 +216,13 @@ d3.csv("data/colleges.csv", function(data) {
  //       });
 
  //    var temp2= chart2.selectAll("circle")
-	//    .data(csv)
+	//    .data(data)
 	//    .enter()
 	//    .append("circle")
 	//    .attr("id",function(d,i) {return "c" + i;} )
 	//    .attr("stroke", "black")
  //       .attr("class", "unselected")
-	//    .attr("cx", function(d) { return xScale2(d.ACT); })
+	//    .attr("cx", function(d) { return xScale2(d.medEarningsAfter8); })
 	//    .attr("cy", function(d) { return yScale2(d.GPA); })
 	//    .attr("r", 5)
 	//    .on("click", function(d,i) {
@@ -272,59 +255,60 @@ d3.csv("data/colleges.csv", function(data) {
  //       });
 
 
- //    chart1 // or something else that selects the SVG element in your visualizations
-	// 	.append("g") // create a group node
-	// 	.attr("transform", "translate(0,"+ (width -30)+ ")")
-	// 	.call(xAxis) // call the axis generator
-	// 	.append("text")
-	// 	.attr("class", "label")
-	// 	.attr("x", width-16)
-	// 	.attr("y", -6)
-	// 	.style("text-anchor", "end")
- //        .style("fill", "black")
-	// 	.text("SATM");
+    chart1 // or something else that selects the SVG element in your visualizations
+		.append("g") // create a group node
+		.attr("transform", "translate(0,"+ (width -30)+ ")")
+		.call(xAxis) // call the axis generator
+		.append("text")
+		.attr("class", "label")
+		.attr("x", width-16)
+		.attr("y", -6)
+		.style("text-anchor", "end")
+        .style("fill", "black")
+		.text("Median Debt");
 
- //    chart1 // or something else that selects the SVG element in your visualizations
-	// 	.append("g") // create a group node
-	// 	.attr("transform", "translate(50, 0)")
-	// 	.call(yAxis)
-	// 	.append("text")
-	// 	.attr("class", "label")
-	// 	.attr("transform", "rotate(-90)")
-	// 	.attr("y", 6)
-	// 	.attr("dy", ".71em")
-	// 	.style("text-anchor", "end")
- //        .style("fill", "black")
-	// 	.text("SATV");
+    chart1 // or something else that selects the SVG element in your visualizations
+		.append("g") // create a group node
+		.attr("transform", "translate(50, 0)")
+		.call(yAxis)
+		.append("text")
+		.attr("class", "label")
+		.attr("transform", "rotate(-90)")
+		.attr("y", 6)
+		.attr("dy", ".71em")
+		.style("text-anchor", "end")
+        .style("fill", "black")
+		.text(selectedValueY);
 
- //    chart2 // or something else that selects the SVG element in your visualizations
-	// 	.append("g") // create a group node
-	// 	.attr("transform", "translate(0,"+ (width -30)+ ")")
-	// 	.call(xAxis2)
-	// 	.append("text")
-	// 	.attr("class", "label")
-	// 	.attr("x", width-16)
-	// 	.attr("y", -6)
-	// 	.style("text-anchor", "end")
- //        .style("fill", "black")
-	// 	.text("ACT");
+    chart2 // or something else that selects the SVG element in your visualizations
+		.append("g") // create a group node
+		.attr("transform", "translate(0,"+ (width -30)+ ")")
+		.call(xAxis2)
+		.append("text")
+		.attr("class", "label")
+		.attr("x", width-16)
+		.attr("y", -6)
+		.style("text-anchor", "end")
+        .style("fill", "black")
+		.text("Median Earnings After 8 Years");
 
- //    chart2 // or something else that selects the SVG element in your visualizations
-	// 	.append("g") // create a group node
-	// 	.attr("transform", "translate(50, 0)")
-	// 	.call(yAxis2)
-	// 	.append("text")
-	// 	.attr("class", "label")
-	// 	.attr("transform", "rotate(-90)")
-	// 	.attr("y", 6)
-	// 	.attr("dy", ".71em")
-	// 	.style("text-anchor", "end")
- //        .style("fill", "black")
-	// 	.text("GPA");
-	// });
+    chart2 // or something else that selects the SVG element in your visualizations
+		.append("g") // create a group node
+		.attr("transform", "translate(50, 0)")
+		.call(yAxis2)
+		.append("text")
+		.attr("class", "label")
+		.attr("transform", "rotate(-90)")
+		.attr("y", 6)
+		.attr("dy", ".71em")
+		.style("text-anchor", "end")
+        .style("fill", "black")
+		.text(selectedValueY);
 
 /**************************************************************************
                             BAR CHART STUFF
 
     We'll need to do the bar chart info ourselves, unfortunately.
 **************************************************************************/
+
+    });
