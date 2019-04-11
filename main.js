875505f3bc215d47d2f8a8ad72f720120805937f
Cozzone, Justin Michael <jcozzone3@gatech.edu>
2019-04-11 08:13:46 -0400

    Removed Comment
    Unneeded

--- main.js
+++ main.js
@@ -401,11 +401,6 @@ d3.csv("data/colleges.csv", function(data) {
 /*****************************************************************************
                             POINTS SETUP
 
-    We'll need if/elses here as well unless there is a much easier way
-    I am missing. However, once again, shouldn't be awful, just a whole
-    bunch of grunt work.
-
-                        WILL ALSO NEED BRUSHING?
 *****************************************************************************/
 
 	 // add scatterplot points
